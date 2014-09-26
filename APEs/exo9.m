0e4aafa5cf525596d69d66a11285a369fdeeaff7
xlambein <xavier.lambein@student.uclouvain.be>
2014-09-26 16:53:32 +0200

    Fixed exo9 ; added exo12

--- APEs/exo9.m
+++ APEs/exo9.m
@@ -2,19 +2,21 @@
 
 function exo9
 syms x
-f3 = cos(x) + sin(x);
 
 estimateError('cosh', (-1 + (0:4)/2))
 estimateError('sinh', (-1 + (0:4)/2))
-estimateError(f3, (-pi/2 + pi*(0:4)/4))
+estimateError(matlabFunction(cos(x) + sin(x)), (-pi/2 + pi*(0:4)/4))
 
 end
 
 function err = estimateError(f, X)
-U = feval(f, X);
+% ESTIMATEERROR  Estimates error between a function and its polynomial
+%                approximation using points (X, f(X)).
 
 x = linspace(X(1), X(end), 1000);
+U = feval(f, X);
 u = feval(f, x);
+
 uh = polyval(polyfit(X, U, length(X)-1), x);
 
 err = max(u-uh);
